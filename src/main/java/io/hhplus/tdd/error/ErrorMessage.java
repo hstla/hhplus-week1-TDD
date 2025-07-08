c8e35b737ed1b1c2abba411304328b7e7fefeb82
hstla <ree7243@gmail.com>
2025-07-08 20:39:08 +0900

    feat: 다양한 에러 메시지를 응답할 수 있도록 로직 수정 및 메시지 추가

---
+++ src/main/java/io/hhplus/tdd/error/ErrorMessage.java
@@ -0,0 +1,6 @@
+package io.hhplus.tdd.error;
+
+public class ErrorMessage {
+	public static final String NEGATIVE_POINT = "포인트는 0 이상이어야 합니다.";
+	public static final String NEGATIVE_USER_ID = "유저 아이디는 0 이상이어야 합니다.";
+}
