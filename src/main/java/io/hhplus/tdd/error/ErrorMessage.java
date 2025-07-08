d2ef54482b88e17e2a0fcb9d4630c75f61e90e5d
hstla <ree7243@gmail.com>
2025-07-08 23:51:23 +0900

    test: 포인트 사용 테스트 코드 작성(#2)
    - 포인트 사용 성공, 포인트 사용 실패 테스트 작성
    - 에러 메시지 추가

--- src/main/java/io/hhplus/tdd/error/ErrorMessage.java
+++ src/main/java/io/hhplus/tdd/error/ErrorMessage.java
@@ -3,4 +3,5 @@ package io.hhplus.tdd.error;
 public class ErrorMessage {
 	public static final String NEGATIVE_POINT = "포인트는 0 이상이어야 합니다.";
 	public static final String NEGATIVE_USER_ID = "유저 아이디는 0 이상이어야 합니다.";
+	public static final String INSUFFICIENT_POINT_MESSAGE = "포인트 잔고가 부족합니다.";
 }
