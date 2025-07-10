8bfe0e4b1aeb61d71d60f8920f979eb6a1795d6e
hstla <ree7243@gmail.com>
2025-07-11 00:00:15 +0900

    test: database 테슽 코드 추가
    데이터베이스 테스트 코드 작성
    DB 초기화를 위해 clear() 메서드 생성

---
+++ src/test/java/io/hhplus/tdd/database/PointHistoryTableTest.java
@@ -0,0 +1,73 @@
+package io.hhplus.tdd.database;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.TransactionType;
+
+class PointHistoryTableTest {
+
+	private final PointHistoryTable pointHistoryTable = new PointHistoryTable();
+
+	@AfterEach
+	void tearDown() {
+		pointHistoryTable.clear();
+	}
+
+	@Test
+	@DisplayName("기존 히스토리가 없으면 빈 리스트를 반환한다.")
+	public void 빈_히스토리_조회() throws Exception {
+	    //given
+		long id = 1L;
+
+	    //when
+		List<PointHistory> histories = pointHistoryTable.selectAllByUserId(id);
+
+		//then
+		assertThat(histories).hasSize(0);
+	}
+
+	@Test
+	@DisplayName("히스토리 생성에 성공한다.")
+	public void 히스토리_생성() throws Exception {
+		//given
+		long id = 1L;
+		long amount = 10_000L;
+		TransactionType type = TransactionType.CHARGE;
+
+		//when
+		long updateMillis = System.currentTimeMillis();
+		PointHistory pointHistory = pointHistoryTable.insert(id, amount, type, updateMillis);
+
+		//then
+		assertThat(pointHistory.id()).isEqualTo(id);
+		assertThat(pointHistory.amount()).isEqualTo(amount);
+		assertThat(pointHistory.type()).isEqualTo(type);
+		assertThat(pointHistory.updateMillis()).isEqualTo(updateMillis);
+	}
+
+	@Test
+	@DisplayName("기존 히스토리를 반환한다.")
+	public void 히스토리_조회() throws Exception {
+	    //given
+		long id = 1L;
+		long amount = 10_000L;
+		TransactionType type = TransactionType.CHARGE;
+
+		pointHistoryTable.insert(id, amount, type, System.currentTimeMillis());
+
+		//when
+		List<PointHistory> histories = pointHistoryTable.selectAllByUserId(id);
+
+		//then
+		assertThat(histories).hasSize(1);
+		assertThat(histories.get(0).amount()).isEqualTo(amount);
+		assertThat(histories.get(0).type()).isEqualTo(type);
+	}
+}
